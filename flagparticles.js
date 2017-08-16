79b2100c11887eedeaaaf0fa05281b05804a7a6b
yash sharma <yash sharma>
2017-08-16 09:16:21 -0400

    hope it works y

--- flagparticles.js
+++ flagparticles.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 
-function onRequest(request, resonse) {
+function onRequest(request, response) {
 	response.writeHead(200,{'content-Type': 'text/html'});
 	fs.readFile('./index.html', null, function(error, data) {
 		if (error) {
