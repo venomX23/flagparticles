6970888622589f47f78b9ee2e49f6958f815d76f
yash sharma <yash sharma>
2017-08-16 09:04:09 -0400

    hope it works

---
+++ flagparticles.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var fs = require('fs');
+
+function onRequest(request, resonse) {
+	response.writeHead(200,{'content-Type': 'text/html'});
+	fs.readFile('./index.html', null, function(error, data) {
+		if (error) {
+			response.writeHead(404);
+			response.write('File Not Found !');
+		} else {
+			response.write(data);
+		}
+		response.end();
+	});
+}
