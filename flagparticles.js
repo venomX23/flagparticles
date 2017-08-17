fd80ae34d59e8e79c0c16691654bb9866cc1a8be
yash sharma <yash sharma>
2017-08-17 09:38:27 -0400

    make it better

--- flagparticles.js
+++ flagparticles.js
@@ -1,10 +1,74 @@
-var express = require('express');
-var app = express();
-var serv = require('http').server(app);
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
 
-app.get('/',function(req, res) {
-	res.sendFile(C:\Users\yash\Desktop\webs + '/client/index.html')
-	});
-	app.use('/client',express.static(C:\Users\yash\Desktop\webs + '/client'));
-	serv.listen(2000);
-	
+function send404(response) {
+  response.writeHead(404, {"Content-type" : "text/plain"});
+  response.write("Error 404: resource not found");
+  response.end();
+}
+function sendPage(response, filePath, fileContents) {
+  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(C:\Users\yash\Desktop\webs\index.html))});
+  response.end(fileContents);
+}
+function serverWorking(response, absPath) {
+  fs.exists(absPath, function(exists) {
+    if (exists) {
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(response)
+        } else {
+          sendPage(response, absPath, data);
+        }
+      });
+    } else {
+      send404(response);
+    }
+  });
+}
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  if (request.url == '/') {
+    filePath = "C:\Users\yash\Desktop\webs\index.html";
+  } else {
+    filePath = "public" + request.url;
+  }
+
+  var absPath = "./" + filePath;
+  serverWorking(response, absPath);
+});
+
+http.createServer(var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("It's alive!");
+  response.end();
+}).listen(3000)
+
+
+var port_number = server.listen(process.env.PORT || 3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
