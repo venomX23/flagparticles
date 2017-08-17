5db71c24c9e0b239004d9bd716c2796e2ffe4ff2
yash sharma <yash sharma>
2017-08-17 08:45:55 -0400

    make it better

--- flagparticles.js
+++ flagparticles.js
@@ -1,15 +1,10 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var serv = require('http').server(app);
 
-function onRequest(request, response) {
-	response.writeHead(200,{'content-Type': 'text/html'});
-	fs.readFile('./index.html', null, function(error, data) {
-		if (error) {
-			response.writeHead(404);
-			response.write('File Not Found !');
-		} else {
-			response.write(data);
-		}
-		response.end();
+app.get('/',function(req, res) {
+	res.sendFile(C:\Users\yash\Desktop\webs + '/client/index.html')
 	});
-}
+	app.use('/client',express.static(C:\Users\yash\Desktop\webs + '/client'));
+	serv.listen(2000);
+	
